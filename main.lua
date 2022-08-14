15cdda12df68d599326808f8529843617024b577
ratedlua <109558813+ratedlua@users.noreply.github.com>
2022-08-14 15:54:05 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,11 +12,8 @@ else
 
 local queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 
-local ontp = game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
-    if State == Enum.TeleportState.Started then
-		queueteleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/ratedlua/shitmaClient/main/main.lua", true))()')
-    end
-end)
+queueteleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/ratedlua/shitmaClient/main/main.lua", true))()')
+
 
 ----------------------------------------------------------------------------------------------------ANTICHEATBYPASS. Not done
 --[[
